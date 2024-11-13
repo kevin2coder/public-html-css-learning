7f666b137f446de9171ca59f3ac5a7f1e8e1024f
kevin2coder <kevin2coder@gmail.com>
2024-11-13 06:13:53 +0000

    Added/finsished exerciseContacts + test

---
+++ src/exerciseContact.js
@@ -0,0 +1,35 @@
+var contacts = [
+    {
+        'firstName': 'Akira',
+        'lastName': 'Laine',
+        'number': '05412395211',
+        'likes': ['Pizza', 'Coding', 'Brownie Points']
+    },
+    {
+        'firstName': 'Harry',
+        'lastName': 'Potter',
+        'number': '04193912943',
+        'likes': ['Hogwarts', 'Magic', 'Hagrid']
+    },
+    {
+        'firstName': 'Sherlock',
+        'lastName': 'Holmes',
+        'number': '04928472933',
+        'likes': ['Intriguing Cases', 'Violin']
+    },
+    {
+        'firstName': 'Kristina',
+        'lastName': 'Vos',
+        'number': 'Unknown',
+        'likes': ['Javascript', 'Gaming', 'Foxes']
+    }
+]
+function lookUpProfile(name, prop) {
+
+}
+lookUpProfile('Sherlock', 'number')
+lookUpProfile('Kristina', 'numbbber')
+lookUpProfile('Sherlock', 'likes')
+lookUpProfile('Sherlockkkk', 'number')
+// Return value with id and prop input. If no id == input then write 'Contact Unknown.'
+// If no property Write 'Information Unknown.'
